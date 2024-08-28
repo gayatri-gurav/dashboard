469c48f2afed3ac47f2738aa024beeb27efe57e0
gayatri-gurav <rentandshow@gmail.com>
2024-08-28 16:04:58 -0500

    added login page

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {FaGoogle} from "react-icons/fa";
+import {FaFacebook} from "react-icons/fa"
 
 const Register = () => {
     return (
@@ -37,6 +39,30 @@ const Register = () => {
                                  <p>Already have an account? <Link className='font-bold' to="/login">Sign In </Link></p>
                              </div>
 
+                        
+                             <div className='w-full flex justify-center items-center mb-3'>
+                                <div className='w-[45%] bg-slate-700 h-[1px]'></div>
+                                 <div className='w-[10%] flex justify-center items-center'>
+                                    <span className='pb-1'>Or</span>
+                                 </div>
+                                 <div className='w-[45%] bg-slate-700 h-[1px]'></div>
+                                </div>
+                                
+
+                         
+                             <div className='flex justify-center items-center gap-3'>
+                               <div className='w-[135px] h-[35px] flex rounded-md bg-orange-700 shadow-lg hover:shadow-orange-700/50
+                               justify-center cursor-pointer items-center overflow-hidden'>
+                                <span><FaGoogle/></span>
+                               </div>
+
+                               <div className='flex justify-center items-center gap-3'>
+                               <div className='w-[135px] h-[35px] flex rounded-md bg-blue-700 shadow-lg hover:blue-700/50
+                               justify-center cursor-pointer items-center overflow-hidden'>
+                                <span><FaFacebook/></span>
+                               </div>
+                             </div>
+                             </div>
                         </form>
                     </div>
                 </div>
