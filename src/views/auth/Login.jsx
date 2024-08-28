7babe0390218b44fb5fbd6d5f0ec7b10c83fbe1f
gayatri-gurav <rentandshow@gmail.com>
2024-08-28 16:29:57 -0500

    Added stateManagement/usestate for login and register page

--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {FaGoogle} from "react-icons/fa";
-import {FaFacebook} from "react-icons/fa"
+import {FaFacebook} from "react-icons/fa";
+import { useState } from 'react';
 
 const Login = () => {
+
+    const [state,setState] = useState({
+        email:"",
+        password:"",
+ })
+
+ const inputHandle =(e) => {
+     setState({
+         ...state,
+         [e.target.name]: e.target.value
+     })
+ }
+
+ const submit = (e) => {
+     e.preventDefault()
+     console.log(state)
+ }
+
+
+
     return (
         <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'>
                 <div className='w-[350px] text-[#ffffff] p-2'>
@@ -11,16 +32,16 @@ const Login = () => {
                         <h2 className='text-xl mb-3 font-bold'>Welcome to Rent&Show.com</h2>
                         <p className='text-sm mb-3 font-medium'>Please Sign In your account</p>
 
-                        <form>
+                        <form onSubmit={submit}>
                           
                             <div className='flex flex-col w-full gap-1 mb-3'>
                                <label htmlFor="email">Email</label>
-                               <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" name='email' placeholder='Email' id='email' required/>
+                               <input onChange={inputHandle} value={state.email} className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" name='email' placeholder='Email' id='email' required/>
                             </div>
 
                             <div className='flex flex-col w-full gap-1 mb-3'>
                                <label htmlFor="password">Password</label>
-                               <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" name='password' placeholder='Password' id='password' required/>
+                               <input onChange={inputHandle} value={state.password} className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" name='password' placeholder='Password' id='password' required/>
                             </div>
 
                             
