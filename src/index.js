409dbb3c65df2589ae385d6621c7d4ed893e5914
gayatri-gurav <rentandshow@gmail.com>
2024-08-29 00:20:57 -0500

    Added react-redux (state menagement library), added authreducer, added provider store in index.js, added toaster in index.js, added useDispatch in adminlogin.jsx to dispatch adminlogin state to authReducer

--- src/index.js
+++ src/index.js
@@ -3,14 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {Toaster} from 'react-hot-toast';
+import store from './store/index';
 const App = lazy(()=> import('./App'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <BrowserRouter>
+<Provider store={store}>
   <Suspense>
     <App />
+    <Toaster
+      toastOption={{
+        position:'top-right',
+        style:{
+          background : '#283046',
+          color:'white'
+        }
+      }}
+    />
   </Suspense>
+</Provider>
 </BrowserRouter>
 );
 
